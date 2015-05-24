f6aac12c9a22a65d241a12ce980e44a07d9596f0
Nxeed <nxeed@ya.ru>
2015-05-24 14:40:24 +0600

    first

---
+++ README.md
@@ -0,0 +1,103 @@
+jQuery VScroller
+===================
+VScroller is a really small jQuery vertical scroller plugin with support for "jquery.mousewheel", "jquery.event.drag" and "jquery.easing".
+
+How to use it:
+-------------
+Load jquery,event.drag and the jQuery vscroller plugin in your html page.
+``` html
+<script src="jquery.min.js"></script>
+<script src="jquery.event.drag.js"></script>
+<script src="jquery.vscroller.js"></script>
+```
+Load the optional jQuery mousewheel plugin for mouse wheel support.
+``` html
+<script src="jquery.mousewheel.min.js"></script>
+```
+
+Create a basic content block as follow.
+``` html
+<div class="vscroller">
+    <div class="vscroller-content">
+        <h3>Lorem ipsum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, enim, voluptate veritatis aut sunt dicta officiis nulla cumque magnam alias qui rem doloremque! Molestias?</p>
+        <h3>Lorem ipsum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, enim, voluptate veritatis aut sunt dicta officiis nulla cumque magnam alias qui rem doloremque! Molestias?</p>
+        <h3>Lorem ipsum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, enim, voluptate veritatis aut sunt dicta officiis nulla cumque magnam alias qui rem doloremque! Molestias?</p>
+        <h3>Lorem ipsum.</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus, enim, voluptate veritatis aut sunt dicta officiis nulla cumque magnam alias qui rem doloremque! Molestias?</p>
+    </div>
+</div>
+```
+Add your own CSS styles to the news ticker.
+``` css
+.vscroller {
+    border: 1px solid gray;
+    border-radius: 4px;
+    position: relative;
+    overflow: hidden;
+    height: 224px;
+}
+
+.vscroller-content {
+    position: relative;
+    padding: 15px;
+    margin-right: 5px;
+}
+
+.vscroller-scroll-body {
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    background: #666;
+    width: 10px;
+    border-radius: 8px;
+    opacity: 0.5;
+    transition: opacity 0.5s;
+    -webkit-transition: opacity 0.5s;
+}
+
+.vscroller-drag .vscroller-scroll-body,
+.vscroller-scroll-body:hover {
+    opacity: 1;
+}
+
+.vscroller-scroll {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: #999;
+    width: 10px;
+    height: 45px;
+    border-radius: 8px;
+    transition: background-color 0.25s;
+    -webkit-transition: background-color 0.25s;
+    z-index: 99;
+    cursor: pointer;
+}
+
+.vscroller-scroll:focus {
+    background: #ccc;
+}
+```
+Call the plugin to start the news ticker.
+``` javascript
+$('.vscroller').vscroller();
+
+```
+Default settings.
+``` javascript
+
+$('.vscroller').vscroller({
+    // mousewheel support
+    mousewheel: false,
+    // easing function
+    easing: 'linear',
+    // mousewheel and keyboard arrows offset pixel count
+    scrollRate: 15,
+    // scrolling animation duration
+    animationDuration: 400
+});
+```
---
+++ jquery.vscroller.js
@@ -0,0 +1,189 @@
+/*
+ * jQuery VScroller v1.0
+ * Created by Nxeed | https://github.com/nxeed
+ */
+
+if (typeof Object.create !== "function") {
+    Object.create = function (obj) {
+        function F() {
+        }
+        F.prototype = obj;
+        return new F();
+    };
+}
+
+(function ($, window, document) {
+    var VScroller = {
+        $elem: null,
+        $content: null,
+        $scrollBody: null,
+        $scroll: null,
+        height: 0,
+        contentHeight: 0,
+        scrollBodyHeight: 0,
+        scrollHeight: 0,
+        contentPaddingBottom: 0,
+        scrollPos: 0,
+        contentPos: 0,
+        options: {},
+        redraw: function () {
+            var base = this;
+            var ret = true;
+
+            if (this.scrollPos < 0) {
+                this.scrollPos = 0;
+                ret = false;
+            }
+
+            if (this.scrollPos > this.scrollBodyHeight - this.scrollHeight) {
+                this.scrollPos = base.scrollBodyHeight - base.scrollHeight;
+                ret = false;
+            }
+
+            var scrollVal = parseInt(this.scrollPos * 100 / (this.scrollBodyHeight - this.scrollHeight));
+            var contentHeight = this.contentHeight - this.contentPaddingBottom - this.height;
+
+            this.$scroll.stop(false, true).animate({top: this.scrollPos + 'px'}, {
+                duration: this.options.animationDuration,
+                easing: this.options.easing
+            });
+            this.$dummy.stop(false, true).animate({top: this.scrollPos + 'px'}, {
+                duration: this.options.animationDuration,
+                easing: this.options.easing
+            });
+
+            this.$content.stop(false, true).animate({top: (contentHeight * scrollVal / 100) * -1 + 'px'}, {
+                duration: this.options.animationDuration,
+                easing: this.options.easing
+            });
+
+            return ret;
+        },
+        init: function (options, el) {
+            var base = this;
+            this.options = $.extend({}, $.fn.vscroller.options, $(el).data(), options);
+
+            this.$elem = $(el);
+            this.$content = $('.vscroller-content', base.$elem);
+            this.$scrollBody = $('<div class="vscroller-scroll-body"></div>');
+            this.$scroll = $('<a href="#" class="vscroller-scroll"></a>');
+            this.$dummy = $('<div class="vscroller-scroll"></div>');
+
+            this.$elem.append(this.$scrollBody);
+            this.$scrollBody.append(this.$scroll);
+            this.$scrollBody.append(this.$dummy);
+
+            this.$dummy.fadeTo(0, 0).hide();
+
+            this.height = this.$elem.outerHeight();
+
+            this.contentHeight = this.$content.outerHeight();
+            this.contentPaddingBottom = parseInt(this.$content.css('paddingBottom'));
+            this.scrollHeight = this.$scroll.outerHeight();
+
+            this.$scrollBody.css('height', this.$elem.height() - parseInt(this.$scrollBody.css('top')) * 2 + 'px');
+            this.scrollBodyHeight = this.$scrollBody.outerHeight();
+
+            var scrollStart = 0;
+
+            var check = function () {
+                if (base.contentHeight > base.height) {
+                    base.$scrollBody.show();
+                    return true;
+                }
+                base.$scrollBody.hide();
+                return false;
+            };
+
+            $(window).bind('resize.vscroller', function () {
+                base.contentHeight = base.$content.outerHeight();
+                base.redraw();
+                check();
+            });
+
+            this.$scrollBody.bind('mousedown.vscroller', function (e) {
+                if (e.button !== 0) {
+                    return false;
+                }
+                e.preventDefault();
+                var pos = parseInt(e.pageY - $(this).offset().top);
+                base.scrollPos = pos;
+                base.redraw();
+                base.$scroll.focus();
+            });
+
+            this.$scroll.bind('dragstart.vscroller', function (e, dd) {
+                setTimeout(function () {
+                    base.$dummy.show();
+                }, 1);
+                scrollStart = base.scrollPos;
+                base.$elem.addClass('vscroller-drag');
+            }).bind('drag.vscroller', function (e, dd) {
+                base.dragDeltaY = dd.deltaY;
+                var pos = scrollStart + dd.deltaY;
+                base.scrollPos = pos;
+                base.redraw();
+            }).bind('dragend.newstape', function (e, dd) {
+                base.$dummy.hide();
+                base.$scroll.focus();
+                base.$elem.removeClass('vscroller-drag');
+            });
+
+            this.$scroll.bind('click.vscroller', function (e) {
+                $(this).focus();
+                e.preventDefault();
+            });
+
+            this.$scroll.bind('keypress.vscroller', function (e) {
+                var jump = base.options.scrollRate;
+
+                if (e.keyCode === 38) {
+                    base.scrollPos = base.scrollPos - jump;
+                } else if (e.keyCode === 40) {
+                    base.scrollPos = base.scrollPos + jump;
+                }
+                base.redraw();
+            });
+
+            this.$dummy.bind('click.vscroller', function (e) {
+                base.$scroll.focus();
+            });
+
+            if (this.options.mousewheel) {
+                this.$elem.bind('mousewheel.vscroller', function (e) {
+                    if (!check()) {
+                        return;
+                    }
+                    var jump = base.options.scrollRate;
+                    base.scrollPos = (e.deltaY > 0) ? base.scrollPos - jump : base.scrollPos + jump;
+                    if (base.redraw()) {
+                        e.preventDefault();
+                    }
+                });
+            }
+
+            check();
+        }
+    };
+
+    $.fn.vscroller = function (options) {
+        return this.each(function () {
+            if ($(this).data("vscroller-init") === true) {
+                return false;
+            }
+
+            $(this).data("vscroller-init", true);
+            var vscroller = Object.create(VScroller);
+            vscroller.init(options, this);
+            $.data(this, "vscroller", vscroller);
+        });
+    };
+
+    $.fn.vscroller.options = {
+        mousewheel: false,
+        easing: 'linear',
+        scrollRate: 15,
+        animationDuration: 400
+    };
+
+}(jQuery, window, document));
